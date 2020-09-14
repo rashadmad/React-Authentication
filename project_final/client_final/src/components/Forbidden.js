4e2174515e5512c662686120f686e61c9ec8d060
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-09-14 00:45:10 -0700

    make forbidden page friendlier

--- project_final/client_final/src/components/Forbidden.js
+++ project_final/client_final/src/components/Forbidden.js
@@ -1,10 +1,14 @@
 import React from 'react';
+import {
+  NavLink
+} from 'react-router-dom';
 
 const Forbidden = () => {
   return(
     <div className="bounds">
       <h1>Forbidden</h1>
       <p>Oh no! You aren't authorized to access this page.</p>
+      <p>If you would like to go further <NavLink to="/signin">sign in</NavLink></p>
     </div>  
   );
 }
