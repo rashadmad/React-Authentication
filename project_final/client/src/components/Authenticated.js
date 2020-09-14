1c5c9e93f3241e5f13168b314fb91099bf58ab95
Rashad Madison <tap-rmadison@Rashads-Air.attlocal.net>
2020-09-14 11:27:24 -0700

    made auth page friendler

--- project_final/client/src/components/Authenticated.js
+++ project_final/client/src/components/Authenticated.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 export default ({ context  }) => {
   const authUser = context.authenticatedUser;
@@ -7,6 +8,7 @@ export default ({ context  }) => {
     <div className="grid-100">
       <h1>{authUser.firstName} {authUser.lastName} is authenticated!</h1>
       <p>Your email is {authUser.emailAddress}.</p>
+      <p>Take look at the <Link to="/">courses</Link></p>
     </div>
   </div>
   );
